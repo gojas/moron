e10f036d5f42e6b88d4caa6bd1bd3c96088cbe81
gojas <gojas@DESKTOP-MGIFPTV>
2017-06-07 15:11:02 +0200

    bla bla

--- Game1/Component/Input/MoronInputComponent.cs
+++ Game1/Component/Input/MoronInputComponent.cs
@@ -13,22 +13,22 @@ namespace Component
 
     public class MoronInputComponent : InputComponent
     {
+        MouseState oldMouseState;
 
         public override void update(GameObject gameObject, Camera camera)
         {
-
             float moronPositionX = gameObject.position.X;
             float moronPositionY = gameObject.position.Y;
 
 
             KeyboardState state = Keyboard.GetState();
             MouseState mouseState = Mouse.GetState();
+            
 
             float mousePositionX = mouseState.Position.X;
             float mousePositionY = mouseState.Position.Y;
 
             Vector2 mousePosition = new Vector2(mousePositionX, mousePositionY);
-
 
 
             if (mouseState.LeftButton == ButtonState.Pressed) {
@@ -43,31 +43,22 @@ namespace Component
                 System.Diagnostics.Debug.Write(mousePosition + "\n");
             }
 
-            if (mouseState.RightButton == ButtonState.Pressed) {
+            if (mouseState.RightButton == ButtonState.Pressed && 
+                oldMouseState.RightButton == ButtonState.Released
+            ) {
                 GameObject rangeWeaponGameObject = gameObject.SceneManager.GameObjectManager.Get(4);
 
                 rangeWeaponGameObject.ComponentContainer.GetScriptComponent().update(gameObject, gameObject.QuadTree, gameObject.SceneManager);
 
                 // gameObject.FireRange();
             }
-                
 
+            
             Command command = CommandFactory.Get(state);
 
-
-
-            
-
-
             command.Update(gameObject);
-
             camera.Position = gameObject.position;
-
-
-            
-
-
-
+            oldMouseState = mouseState;
         }
     }
 }
