2b24116a40bca77a7d720d1fb77a245fcd91c4cb
gojas <gojas@DESKTOP-MGIFPTV>
2017-06-12 23:35:03 +0200

    bla

--- Game1/Component/Graphic/ExplosionGraphicComponent.cs
+++ Game1/Component/Graphic/ExplosionGraphicComponent.cs
@@ -3,66 +3,28 @@
 namespace Component
 {
     using World.GameObject;
-    using World.GameObject.State.Provider;
     using Texture;
     using Texture.GameObjectTextureDefinition;
-    using System;
     using Texture.Animation;
-    using System.Timers;
 
     public class ExplosionGraphicComponent : GraphicComponent
     {
-
-        int SpriteIndex = 0;
-
-        private float timeElapsed = 0;
-
-        private bool IsLooping = true;
-        private float timeToUpdate = 1f; // 1 second
-        public int FramesPerSecond
-        {
-            set { timeToUpdate = (1f / value); }
-        }
-        
+        Animation animation;
 
         public override void update(GameObject gameObject, SpriteRender spriteRender, GameTime gameTime, float depth)
         {
-            string gameObjectStateString = GameObjectStateProvider.GetStateString(gameObject);
-
-
             ExplosionStanding explosionTextureDefinition = new ExplosionStanding();
-            timeElapsed += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            int totalSpritesToRender = explosionTextureDefinition.GetSriteTotal();
+            if (animation == null) {
+                animation = new Animation(spriteRender, gameTime);
 
-            if (timeElapsed > timeToUpdate) {
-                timeElapsed -= timeToUpdate;
-
-                if (SpriteIndex < totalSpritesToRender)
+                for (int i = 0; i < explosionTextureDefinition.GetSriteTotal(); i++)
                 {
-                    SpriteIndex++;
-                }
-                else if (IsLooping) {
-                    SpriteIndex = 0;
-
-                    // gameObject.Destroyed = true;
+                    animation.AddSprite(gameObject.AnimationContainer.GetSpriteBasedOnTextureDefinitionAndIndex(explosionTextureDefinition, i));
                 }
             }
-
-
-
-            // Animation animation = new Animation(spriteRender, gameTime);
-
-            // for (int i = 0; i < explosionTextureDefinition.GetSriteTotal(); i++)
-            // {
-            //    animation.AddSprite(gameObject.AnimationContainer.GetSpriteBasedOnTextureDefinitionAndIndex(explosionTextureDefinition, i));
-            // }
-            // animation.Play();
-            
-
-            Sprite sprite = gameObject.AnimationContainer.GetSpriteBasedOnTextureDefinitionAndIndex(explosionTextureDefinition, SpriteIndex);
-
-            spriteRender.Draw(sprite, gameObject.position);
+                
+            animation.Play();
         }
 
     }
